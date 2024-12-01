9b5297bc441ad41283890b6bc735c366d3114e42
Justin Hall <ktstephano@gmail.com>
2024-12-01 15:31:05 -0700

    Adds more div by zero tests; adds errIO test

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -36,10 +36,22 @@ func runAndEnsureSpecificShutdown(t *testing.T, vm *VM, errcode error) {
 }
 
 var (
-	divByZeroTest = `
+	divByZeroTest1 = `
 		const 0
 		const 1
 		divi
+	`
+
+	divByZeroTest2 = `
+		const 0
+		const 1
+		remu
+	`
+
+	divByZeroTest3 = `
+		const 0
+		const 1
+		rems
 	`
 
 	stackOverflowTest = `
@@ -50,15 +62,25 @@ var (
 
 	illegalInstrTest = `
 		const 1
-		srstore 32	// set CPU mode to non-privileged
+		srstore 32	        // set CPU mode to non-privileged
 		write 0 0
 	`
 
 	unknownInstrTest = `
 		const 0xFFFFFFFF
 		const 0x00
-		storep32
-		jmp 0x00
+		storep32			// store max uint at mem location 0x00
+		jmp 0x00			// jump to instruction at mem location 0x00
+	`
+
+	errIOTest = `
+	loop:
+		// set up a character input request from console IO device
+		const 0             // no input data
+		const 0             // unused interaction id
+		write 3 4           // port 3 = console IO device, command 4 = read 32-bit character
+		pop 4               // get rid of write result
+		jmp loop
 	`
 )
 
@@ -72,7 +94,13 @@ func TestVM(t *testing.T) {
 	vm = compileAndCheck(t, "../examples/runtime.b", "../examples/helloworld.b")
 	runAndEnsureSpecificShutdown(t, vm, errSystemShutdown)
 
-	vm = compileAndCheckSource(t, divByZeroTest)
+	vm = compileAndCheckSource(t, divByZeroTest1)
+	runAndEnsureSpecificShutdown(t, vm, errDivisionByZero)
+
+	vm = compileAndCheckSource(t, divByZeroTest2)
+	runAndEnsureSpecificShutdown(t, vm, errDivisionByZero)
+
+	vm = compileAndCheckSource(t, divByZeroTest3)
 	runAndEnsureSpecificShutdown(t, vm, errDivisionByZero)
 
 	vm = compileAndCheckSource(t, stackOverflowTest)
@@ -83,4 +111,7 @@ func TestVM(t *testing.T) {
 
 	vm = compileAndCheckSource(t, unknownInstrTest)
 	runAndEnsureSpecificShutdown(t, vm, errUnknownInstruction)
+
+	vm = compileAndCheckSource(t, errIOTest)
+	runAndEnsureSpecificShutdown(t, vm, errIO)
 }
